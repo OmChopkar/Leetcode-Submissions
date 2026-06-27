458d63124d93b651e5ede6e0fce6e9a16c5ce181
Om Dhanraj Chopkar <166698655+OmChopkar@users.noreply.github.com>
2026-06-27 21:48:35 +0530

    Time: 13 ms (90.75%), Space: 12.5 MB (67.49%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) == len(t):
+            dict_s = {}
+            dict_t = {}
+        
+            for i in s:
+                if i in dict_s:
+                    dict_s[i] += 1
+                else:
+                    dict_s[i] = 1
+        
+            for i in t:
+                if i in dict_t:
+                    dict_t[i] += 1
+                else:
+                    dict_t[i] = 1
+        
+            if dict_s == dict_t:
+                return True
+            else:
+                return False
+        else:
+            return False
