071380cc17d20fee15bae70c66071c2b7d45fb60
Om Dhanraj Chopkar <166698655+OmChopkar@users.noreply.github.com>
2026-06-30 23:09:48 +0530

    Time: 105 ms (20.22%), Space: 37.5 MB (5.29%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        dic={}
+        for i in nums:
+            if i in dic:
+                dic[i]+=1
+            else:
+                dic[i]=1
+        for i,j in dic.items():
+            if j>=2:
+                return i
+        
