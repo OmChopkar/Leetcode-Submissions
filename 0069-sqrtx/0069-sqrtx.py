15c7b75dc32186dc01099c86249b0fd78cb98693
Om Dhanraj Chopkar <166698655+OmChopkar@users.noreply.github.com>
2026-08-23 22:08:26 +0530

    Time: 0 ms (100%), Space: 12.4 MB (55.22%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def mySqrt(self, x):
+        if x<2:
+            return x
+        low, high = 1, x//2
+        ans=0
+        while low<=high:
+            mid=(low+high)//2
+            if mid*mid<=x:
+                ans=mid
+                low=mid+1
+            else:
+                high=mid-1
+        return ans            
